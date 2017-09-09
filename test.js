c35fd8ec2d96761732898b555a62f0399bad6c04
majkysuk <majkysuk@users.noreply.github.com>
2017-09-09 19:46:51 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,31 @@
+function tryit(main) {
+	var x = "5" , y= 10 ; 
+	z = + x + y ;
+document.write(z);
+}
+
+var array(6) = [2,4,6,1,3,5];
+
+function max (array) {
+	return Math.max.apply( Math, array );
+	
+	alert (max);
+}
+
+function www(arr){
+
+var arr = new Array(5), i;
+
+	for(i = 0; i < arr.length; i++) {
+
+	    arr[i] = +prompt('Enter number');
+
+	}
+
+
+	var max = Math.max.apply(null, arr);
+
+	 
+	document.write(max);
+
+}
