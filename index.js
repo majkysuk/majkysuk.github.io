3b312c8d99f87ba7f7034600df281aa54d1892a3
majkysuk <majkysuk@users.noreply.github.com>
2017-09-09 11:14:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//705392
+
+var city = 705392;
+city.addEventListener("change", function() {
+
+	var URL = "http://api.openweathermap.org/data/2.5/weather?id=";
+	var API_KEY = "d75da5829ee2244d5a25bbb10c1a7318";
+	var FULL_URL = URL + city.value + "&APPID=" + API_KEY;
+
+	var XHR = new XMLHttpRequest();
+	XHR.open("GET", FULL_URL, true);
+	XHR.send();
+
+	XHR.onreadystatechange = function() {
+		if ((XHR.readyState === 4) && (XHR.status === 200)) {
+			var weather = document.getElementById("weather");
+			weather.innerHTML = "";
+			var data = JSON.parse(XHR.responseText);
+			var ul = document.createElement("ul");
+			var li = document.createElement("li");
+			li.innerHTML = "<span>Temperature: " + (data.main.temp - 273.15) + "<sup>0</sup>C</span>";
+			ul.appendChild(li);
+
+			li = document.createElement("li");
+			li.innerHTML = "<span>Humidity: " + data.main.humidity + " %</span>";
+			ul.appendChild(li);
+
+			weather.appendChild(ul);
+		}
+	};
+
+
+	
+}, false);
